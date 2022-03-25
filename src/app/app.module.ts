6842551783a80e849a85553b12543dc66258db65
JohanTellez <johanatellezc@gmail.com>
2022-03-25 18:45:41 -0500

    gifs app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { GiftsModule } from './gifts/gifts.module';
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    GiftsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
